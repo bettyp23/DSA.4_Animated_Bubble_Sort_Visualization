acdd5290a1aaaac42393e9b76f9444688b62d3fc
Betty Phipps <140215849+bettyp23@users.noreply.github.com>
2025-06-06 11:45:56 -0400

    Update main.cpp
    starting bubble sort

--- main.cpp
+++ main.cpp
@@ -10,6 +10,32 @@ using namespace std;
 //function to print array stack
 
 //Bubble sort with animation
+void bubbleSort(vector<int>& arr){
+  int n = arr.size()
+  const int sTime= 1000000;  //animation delay per step in microseconds
+  int sCounter= 0;
+
+  auto start = high_resolution_clock::now();    //start time
+
+  for (int i = 0; i < n - 1; i++){
+    for (int j = 0; j < n - i - 1; j++){
+      print(arr, j, j + 1);
+      usleep(sTime);        //animation delay
+      sCounter++;
+      if (arr[j], arr[j + 1]){
+        swap(arr[j], arr[j + 1]);
+      }
+    }
+  }
+  auto end = high_resolution_clock::now();      //end time
+  print(arr);
+
+  //calculating total time and subtract visualization delay
+  duration<double> totalTime = end - start;
+  double adjustedTime = totalTime.count() - (sCounter * sTime / 1000000.0);
+
+  cout<<adjustedTime<<endl;
+}
 
 //Function to generate an array 'n' integers with given 'sorted'
 vector<int> generate(int n, int sorted){
