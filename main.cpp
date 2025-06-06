d581b28e5b7400bb6fd79fca55706bf067b4d387
Betty Phipps <140215849+bettyp23@users.noreply.github.com>
2025-06-06 10:34:19 -0400

    Create main.cpp
    Setup and adding libraries, main first levels

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <algorithm>
+#include <chrono>
+#include <cstdlib>
+
+using namespace std;
+
+//function to print array stack
+
+//Bubble sort with animation
+
+//Function to generate specific sorted numbers
+
+
+int main(){
+srand(time(0))
+int size[] = {10, 20, 30, 40};
+int level[] = {0, 25, 50, 75};
+}
