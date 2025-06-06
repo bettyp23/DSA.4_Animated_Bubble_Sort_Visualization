daa830a078c434dd837e4dc16f9917800eb4101f
Betty Phipps <140215849+bettyp23@users.noreply.github.com>
2025-06-06 10:50:20 -0400

    Update main.cpp
    Function to generate array of n integers

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
-#include <algorithm>
-#include <chrono>
-#include <cstdlib>
+#include <algorithm>  //for sort
+#include <chrono>     //rand and srand
+#include <cstdlib>    //time
 
 using namespace std;
 
@@ -11,11 +11,21 @@ using namespace std;
 
 //Bubble sort with animation
 
-//Function to generate specific sorted numbers
+//Function to generate an array 'n' integers with given 'sorted'
+vector<int> generate(int n, int sorted){
+  vector<int> arr(n);
 
+  //fill arr with random values 1-10
+  for (int& num : arr){
+    num = rand() % 10 + 1;
+  }
+
+  int sortedPer = (n * sorted) / 100;           //calculate how many elements to be sorted
+  sort(arr.begin(), arr.begin() + sortedPer);   //sort first 'sortedPer' elements
+}
 
 int main(){
-srand(time(0))
-int size[] = {10, 20, 30, 40};
-int level[] = {0, 25, 50, 75};
+  srand(time(0))    //generate and print arrays for testing
+  int size[] = {10, 20, 30, 40};
+  int level[] = {0, 25, 50, 75};
 }
